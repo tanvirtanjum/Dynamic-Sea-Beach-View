761d3dc4a696b5d79e6671f50ce5c64603efd049
Ashfaq Afzal Chowdhury Fahim <aacfahim@gmail.com>
2020-12-30 01:19:53 +0600

    crab structure

--- main.cpp
+++ main.cpp
@@ -146,6 +146,119 @@ void load_Restart(int x)
     glutDisplayFunc(load_Start);
 }
 
+static void drawCrabGrip() {
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, crabGripColour);
+	glBindTexture(GL_TEXTURE_2D, getTexture(GKOM_TEX_CRAB_GRIP));
+
+	glPushMatrix();
+		glScalef(1.0f, 1.0f, 0.7f);
+		glRotatef(45.0f, 0.0f, 1.0f, 0.0f);
+		glScalef(0.7f, 0.3f, 0.7f);
+
+		GLdouble cuttingPlaneX[4] = { 1.0, 0.0, 0.0, 0.0 };
+		glClipPlane(GL_CLIP_PLANE0, cuttingPlaneX);
+
+		glEnable(GL_CLIP_PLANE0);
+			glPushMatrix();
+				glTranslatef(0.0f, 10.0f, 0.0f);
+				glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
+				glutSolidSphere(12.0f, 64, 64);
+				glScalef(1.0f, 0.2f, 1.0f);
+				GLUquadricObj *sphereQuadratic1 = gluNewQuadric();
+				gluQuadricOrientation(sphereQuadratic1, GLU_INSIDE);
+				gluQuadricNormals(sphereQuadratic1, GLU_SMOOTH);
+				gluQuadricTexture(sphereQuadratic1, GLU_TRUE);
+				gluSphere(sphereQuadratic1, 12.0, 64, 64);
+				gluDeleteQuadric(sphereQuadratic1);
+			glPopMatrix();
+		glDisable(GL_CLIP_PLANE0);
+
+		glTranslatef(0.0f, 10.0f, 0.0f);
+
+		float animationAngle = 30.0f * sin(frame * 0.01f);
+		glRotatef(animationAngle, 0.0f, 1.0f, 0.0f);
+
+		GLdouble cuttingPlaneY[4] = { 0.0, 0.0, 1.0, 0.0 };
+		glClipPlane(GL_CLIP_PLANE1, cuttingPlaneY);
+
+		glEnable(GL_CLIP_PLANE1);
+			glPushMatrix();
+				glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
+				glutSolidSphere(11.8f, 64, 64);
+				glScalef(1.0f, 1.0f, 0.2f);
+				GLUquadricObj *sphereQuadratic2 = gluNewQuadric();
+				gluQuadricOrientation(sphereQuadratic2, GLU_INSIDE);
+				gluQuadricNormals(sphereQuadratic2, GLU_SMOOTH);
+				gluQuadricTexture(sphereQuadratic2, GLU_TRUE);
+				gluSphere(sphereQuadratic2, 11.8, 64, 64);
+				gluDeleteQuadric(sphereQuadratic2);
+			glPopMatrix();
+		glDisable(GL_CLIP_PLANE1);
+	glPopMatrix();
+}
+
+static void drawCrabLegs() {
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, crabLegsColour);
+
+	drawCrabLeg();
+
+	glPushMatrix();		
+		glRotatef(20, 0.0f, 1.0f, 0.0f);
+		glTranslatef(-0.5f, 0.0f, 7.0f);
+		drawCrabLeg();
+	glPopMatrix();
+
+	glPushMatrix();
+		glRotatef(-20, 0.0f, 1.0f, 0.0f);
+		glTranslatef(0.5f, 0.0f, -7.0f);
+		drawCrabLeg();
+	glPopMatrix();
+}
+
+void drawCrabLeg() {
+	glPushMatrix();
+		glBindTexture(GL_TEXTURE_2D, getTexture(GKOM_TEX_CRAB_LEG));
+
+		glScalef(1.0f, 1.0f, 0.6f);
+			glTranslatef(0.0f, 10.6f, 0.0f);
+		glRotatef(90, 1.0f, 0.0f, 0.0f);
+
+		glPushMatrix();
+			glTranslatef(0.0f, 0.0f, 6.5f);
+			GLUquadricObj *diskQuadratic2 = gluNewQuadric();
+			gluQuadricNormals(diskQuadratic2, GLU_SMOOTH);
+			gluQuadricTexture(diskQuadratic2, GLU_TRUE);
+			gluDisk(diskQuadratic2, 0.0f, 1.0f, 64, 4);
+			gluDeleteQuadric(diskQuadratic2);
+		glPopMatrix();
+
+		glPushMatrix();
+			GLUquadricObj *cylinderQuadratic1 = gluNewQuadric();
+			gluCylinder(cylinderQuadratic1, 0.8f, 1.0f, 6.5f, 64, 64);
+			gluDeleteQuadric(cylinderQuadratic1);
+		glPopMatrix();
+
+		glPushMatrix();
+			glTranslatef(0.0f, 0.0f, 6.3f);
+			glRotatef(15, 0.0f, 1.0f, 0.0f);
+			glutSolidCone(0.9f, 4.0f, 64, 64);
+		glPopMatrix();
+
+		glTranslatef(0.2f, 0.0f, 0.1f);
+		glRotatef(70, 0.0f, 1.0f, 0.0f);
+
+		glPushMatrix();
+			glutSolidSphere(1.1f, 64, 64);
+		glPopMatrix();
+
+		glPushMatrix();
+			GLUquadricObj *cylinderQuadratic2 = gluNewQuadric();
+			gluCylinder(cylinderQuadratic2, 1.1f, 0.7f, 10.5f, 64, 64);
+			gluDeleteQuadric(cylinderQuadratic2);
+		glPopMatrix();
+	glPopMatrix();
+}
+
 void Ship()
 {
     glScalef(.2,.2,0);
